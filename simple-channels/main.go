5ec50b54f2ff3e89403c3069fb57c1372e05d578
LeeTrent <lee.trent.1@gmail.com>
2023-02-07 06:15:05 -0600

    Done with simple channels

--- simple-channels/main.go
+++ simple-channels/main.go
@@ -5,36 +5,65 @@ import (
 	"strings"
 )
 
+// shout has two parameters: a receive only chan ping, and a send only chan pong.
+// Note the use of <- in function signature. It simply takes whatever
+// string it gets from the ping channel,  converts it to uppercase and
+// appends a few exclamation marks, and then sends the transformed text to the pong channel.
 // ping is a 'receive-only' channel (ping <-chan string)
 // pong is a 'send-only' channel (pong chan<-string)
 func shout(ping <-chan string, pong chan<- string) {
 	for {
+
+		// s, ok := <-ping
+		// if !ok {
+		// 	fmt.Println("channel ping is either closed or empty.")
+		// }
+
+		// read from the ping channel. Note that the GoRoutine waits here
+		// -- it blocks until something is received on this channel.
 		s := <-ping
 		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
 	}
 }
 
 func main() {
-	// Create 2 channels
+	// Create two channels. Ping is what we send to, and pong is what comes back.
 	ping := make(chan string)
 	pong := make(chan string)
+
+	// start a goroutine
 	go shout(ping, pong)
 
 	fmt.Println("Type something and press ENTER (enter Q to quit)")
 	for {
+		// Print a prompt
 		fmt.Print("->")
+
+		// Get user input
 		var userInput string
 		_, _ = fmt.Scanln(&userInput)
+
 		if userInput == strings.ToLower("q") {
+			// Jump out of for loop
 			break
 		}
+		// send userInput to "ping" channel
 		ping <- userInput
-		response := <-pong
-		fmt.Println("Response:", response)
 
+		// Wait for a response from the pong channel.
+		// Again, program blocks (pauses) until it receives something
+		// from that channel.
+		response := <-pong
+
+		// Print the response to the console.
+		fmt.Println("Response:", response)
 	}
 
-	fmt.Println("All done. Closing channels.")
+	// Print the response to the console.
+	// fmt.Println("All done. Closing channels.")
 	close(ping)
 	close(pong)
+
+	// close(pong)
+	// close(ping)
 }
